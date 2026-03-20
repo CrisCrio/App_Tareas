74da1e29ef0abd83cffe563baf5f42c9bc0ee68d
CrisCrio <jaramartinezcristianesteban@gmail.com>
2026-03-20 17:52:39 -0500

    actualizacion de App

--- App.js
+++ App.js
@@ -1,41 +1,56 @@
+import React, { useContext } from 'react';
+import { StyleSheet, View, ActivityIndicator } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React, { Activity } from 'react';
-import { AuthProvider } from "./src/context/authContext";
-import LoginScreen from "./src/screens/LoginScreens";
+import { AuthContext, AuthProvider } from "./context/authContext";
+import LoginScreen from "./screens/LoginScreen";
+import HomeScreen from "./screens/HomeScreen";
+import TasksScreen from "./screens/TasksScreen";
 
-// componente interno para manejar la logica de carga
-// dentro de rootNavigator en App.js
+const Stack = createStackNavigator();
+
 const RootNavigator = () => {
-  const { userToken, isLoading } = React.useContext(AuthContext);
+    const { userToken, isLoading } = useContext(AuthContext);
 
-  if (isLoading) {
+    if (isLoading) {
+        return (
+            <View style={styles.loadingContainer}>
+                <ActivityIndicator size="large" color="#0000ff" />
+            </View>
+        );
+    }
+
     return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <ActivityIndicator size="large" color="#0000ff" />
-      </View>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            {userToken ? (
+                <>
+                    <Stack.Screen name="Home" component={HomeScreen} />
+                    <Stack.Screen name="Tasks" component={TasksScreen} />
+                </>
+            ) : (
+                <Stack.Screen name="Login" component={LoginScreen} />
+            )}
+        </Stack.Navigator>
     );
-  }
-
-  // LOGICA DE NAVEGACION: Si hay token, muestra tareas, sino login
-  // Si existe userToken, mostramios la pantalla de tareas.
-  // Si no, mostramos la pantalla de login.
-  return userToken ? <TasksScreen /> : <LoginScreen />;
 };
 
 export default function App() {
-  return (
-    <AuthProvider>
-      <RootNavigator />
-    </AuthProvider>
-  );
+    return (
+        <AuthProvider>
+            <NavigationContainer>
+                <StatusBar style="auto" />
+                <RootNavigator />
+            </NavigationContainer>
+          </AuthProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    loadingContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
 });
